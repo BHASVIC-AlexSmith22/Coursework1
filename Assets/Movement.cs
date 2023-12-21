2b3eeefa08cb40b89cdb9a5d208ac4298382accb
BHASVIC-AlexSmith22 <137897653+BHASVIC-AlexSmith22@users.noreply.github.com>
2023-12-21 22:03:13 +0000

    reset to start obstacles
    working reset to start when touching obstacles, still need to add kill velocity

--- Assets/Movement.cs
+++ Assets/Movement.cs
@@ -6,9 +6,19 @@ public class Movement : MonoBehaviour
 {
     public Rigidbody body;
     public float countdownOne;
+    public int SideSpeed = 300;
+    public int ForwardVelocity = 8;
+    public int JumpHeight = 200;
+    public float DuckHeight = 0.5f;
+
+    public void Death()
+    {
+        Debug.Log("Death called");
+        transform.position = new Vector3(0, 1, 0);
+    }
     public void Jump()
     {
-        body.AddForce(new Vector3(0, 200f, 0));
+        body.AddForce(new Vector3(0, JumpHeight, 0));
         countdownOne = 1;
         Invoke("WaitOneSec", 1);
     }
@@ -32,12 +42,12 @@ public class Movement : MonoBehaviour
     {
         if (Input.GetKey("d"))
         {
-            body.AddForce(new Vector3(2, 0, 0));
+            body.AddForce(new Vector3(SideSpeed * Time.deltaTime, 0, 0));
         }
 
         if (Input.GetKey("a"))
         {
-            body.AddForce(new Vector3(-2, 0, 0));
+            body.AddForce(new Vector3(-SideSpeed * Time.deltaTime, 0, 0));
         }
         if (Input.GetKey("w") & countdownOne ==0 & body.transform.position.y >= 1)
         {
@@ -45,14 +55,14 @@ public class Movement : MonoBehaviour
            
         }
         if (Input.GetKey("s") & countdownOne == 0) {
-           transform.localScale = transform.localScale * 0.5f;
+           transform.localScale = transform.localScale * DuckHeight;
             countdownOne = 1;
             Invoke("UnDuck", 0.5f);
             Invoke("WaitOneSec",1);
          
         }
         //Constant foward movement
-        transform.position += transform.forward * (8 * Time.deltaTime);
+        transform.position += transform.forward * (ForwardVelocity * Time.deltaTime);
     }
 
 }
---
+++ Assets/Obstacles.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Obstacles : MonoBehaviour
+{
+    // Start is called before the first frame update
+    Movement kill;
+    void Start()
+    {
+        kill = GameObject.FindGameObjectWithTag("Player").GetComponent<Movement>();
+    }
+    void OnCollisionEnter(Collision Play)
+    {
+        if (Play.gameObject.name == "Player")
+        {
+            
+            kill.Death();
+        }
+        
+    }
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
